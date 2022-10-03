f705a7f166a4e9c4deb72e80eef5ed00f1eaa4e3
poonam5057 <poonam.maurya.5057@gmail.com>
2022-10-03 15:24:53 +0530

    first commit

---
+++ android/app/src/main/java/com/koli/eos/MainActivity.java
@@ -0,0 +1,24 @@
+package com.koli.eos;
+
+import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen; // here
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "EasyOnService";
+  }
+  @Override
+protected void onCreate(Bundle savedInstanceState) {
+          SplashScreen.show(this);  // here
+
+  super.onCreate(null);
+}
+
+}
