a35006484df780033052dd1d6ce922f0ee877c52
Jagadish5057 <jagadish.pawar.5057@gmail.com>
2022-11-11 17:22:51 +0530

    git initial commit dev_jagdish

---
+++ android/app/build/generated/source/buildConfig/debug/com/koli/eos/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.koli.eos;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.koli.eos";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String APP_NAME = "boilerplate";
+  // Field from default config.
+  public static final String CodePushDeploymentKey = "Bjp_309SmaSFZqRJIvysil1dpyTni5XyfdpsN";
+  // Field from default config.
+  public static final String CodePushDeploymentKeyAndroid = "JCLRuHYLYHcLe1XdhxBrjRsmYOgGa6lx12HIy";
+}
